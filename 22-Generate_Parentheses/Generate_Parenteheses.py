a648669187e0e419129169244f00348dcc699c3a
kimgeonhee317 <rlarjsgml317@gmail.com>
2021-06-26 17:05:20 +0900

    solved 22

---
+++ 22-Generate_Parentheses/Generate_Parenteheses.py
@@ -0,0 +1,37 @@
+class Solution:
+    def generateParentheses(self, n: int) -> list[str]:
+        sliceGroup : list[str] = []
+        Solution.addParentheses(sliceGroup,"", n ,n)
+        
+        target : list[str] = []
+        for slice in sliceGroup:
+            if Solution.isParentheses(slice) == True:
+                target.append(slice)
+        return target
+
+    # step 1 : make all parenthesis case using number
+    def addParentheses(sliceGroup, str: str, left, right):
+        if (left == 0 and right == 0):
+            sliceGroup.append(str)
+            return True
+        elif (left == 0):
+            Solution.addParentheses(sliceGroup, str+')', left, right-1)
+        elif (right == 0): 
+            Solution.addParentheses(sliceGroup, str+'(', left-1, right)
+        else :
+            Solution.addParentheses(sliceGroup, str+')', left, right-1)
+            Solution.addParentheses(sliceGroup, str+'(', left-1, right)
+        
+    # step 2 : filtering out the non-Parentheses case   
+    def isParentheses(str: str) -> bool:
+        flag = 0
+        for slice in str:
+            if slice == '(':
+                flag +=1 
+            else :
+                flag -=1
+                if (flag < 0): return False
+        return True
+
+number : int = 3
+print(Solution.generateParentheses(Solution, number))
